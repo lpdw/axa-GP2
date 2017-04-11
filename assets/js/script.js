8154e93661634256ea10c57f070184597dcd039c
ValentinKajdan <valentin.kajdan@orange.fr>
2017-04-11 12:03:11 +0200

    scrip

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ window.onload = function(){
 
   var itemgroupe = document.getElementById("itemgroupe");
   var dropdown = document.getElementById("dropdown-groupe");
+  var chevron = document.getElementById("chevron");
+  console.log(chevron.src);
 
   itemgroupe.onclick = function() {
     var style = window.getComputedStyle(dropdown),
@@ -9,8 +11,10 @@ window.onload = function(){
 
     if (display == "none") {
       dropdown.style.display = "block";
+      chevron.src = "assets/img/chevron-down.png";
     } else {
       dropdown.style.display = "none";
+      chevron.src = "assets/img/chevron.png";
     }
   };
 
