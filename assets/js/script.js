6942ad065c3f6438ed675bfdf822e2ec58d0d2ae
ValentinKajdan <valentin.kajdan@orange.fr>
2017-04-12 12:19:17 +0200

    partie one document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,43 @@
 window.onload = function(){
 
-  var itemgroupe = document.getElementById("itemgroupe");
-  var dropdown = document.getElementById("dropdown-groupe");
-  var chevron = document.getElementById("chevron");
-  console.log(chevron.src);
+  var item = document.getElementsByClassName('header-item');
+  var checkbox = document.getElementsByClassName('checkbox');
 
-  itemgroupe.onclick = function() {
-    var style = window.getComputedStyle(dropdown),
-    display = style.getPropertyValue('display');
+    for(var i = 0; i < item.length; i++) {
+        var anchor = item[i];
+        anchor.onclick = function() {
+          var dropdown = this.nextSibling.nextSibling;
+          var style = window.getComputedStyle(dropdown),
+          display = style.getPropertyValue('display'),
+          chevron = this.querySelector(".chevron");
+          console.log(chevron);
 
-    if (display == "none") {
-      dropdown.style.display = "block";
-      chevron.src = "assets/img/chevron-down.png";
-    } else {
-      dropdown.style.display = "none";
-      chevron.src = "assets/img/chevron.png";
+          if (display == "none") {
+            dropdown.style.display = "block";
+            chevron.classList.add('rotate');
+          } else {
+            dropdown.style.display = "none";
+            chevron.classList.remove('rotate');
+          }
+        };
     }
-  };
 
+    // Check all inputs
+    for(var j = 0; j < checkbox.length; j++) {
+      var cas = checkbox[j];
+      cas.onchange = function() {
+        var checkDrop = this.parentElement.nextSibling.nextSibling;
+        var inputs = checkDrop.querySelectorAll("input"),
+        k = 0;
+        if (this.checked === true) {
+          for (k = 0; k < inputs.length; k++) {
+            inputs[k].checked = true;
+          }
+        } else {
+          for (k = 0; k < inputs.length; k++) {
+            inputs[k].checked = false;
+          }
+        }
+      };
+    }
 };
