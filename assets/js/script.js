3566e3c77169de54668e996d405f172249a2d308
ValentinKajdan <valentin.kajdan@orange.fr>
2017-04-14 14:37:02 +0200

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,92 @@
-window.onload = function(){
+window.addEventListener("load", function(){
 
-  var item = document.getElementsByClassName('header-item');
-  var checkbox = document.getElementsByClassName('checkbox');
-  var datasize = 0;
+  var items = [].slice.call(document.getElementsByClassName('header-item')),
+  checkbox = [].slice.call(document.getElementsByClassName('checkbox')),
+  datasize = 0;
 
-    for(var i = 0; i < item.length; i++) {
-        var anchor = item[i];
-        anchor.onclick = function() {
-          var dropdown = this.nextSibling.nextSibling;
-          var style = window.getComputedStyle(dropdown),
-          display = style.getPropertyValue('display'),
-          chevron = this.querySelector(".chevron");
+    // Accordion
+    items.forEach(function(item) {
+      item.addEventListener("click", function(){
+        var dropdown = this.nextSibling.nextSibling;
+        var style = window.getComputedStyle(dropdown),
+        display = style.getPropertyValue('display'),
+        chevron = this.querySelector(".chevron");
 
-          if (display == "none") {
-            dropdown.style.display = "block";
-            chevron.classList.add('rotate');
-          } else {
-            dropdown.style.display = "none";
-            chevron.classList.remove('rotate');
-          }
-        };
-    }
+        chevron.classList.toggle('rotate');
+        dropdown.classList.toggle('hide');
+      });
+    });
+
 
     // Check all inputs
-    for(var j = 0; j < checkbox.length; j++) {
-      var cas = checkbox[j];
-      cas.onchange = function() {
-
-
+    checkbox.forEach(function(box){
+      box.addEventListener("change", function(){
         var checkDrop = this.parentElement.nextSibling.nextSibling;
         var inputs = checkDrop.querySelectorAll("input"),
         k = 0;
-        if (this.checked === true) {
+        if (this.checked) {
+          box.nextSibling.nextSibling.querySelector('.checked .check').classList.add("showimg");
+          box.nextSibling.nextSibling.querySelector('.checked .check').classList.remove("hideimg");
+          box.nextSibling.nextSibling.querySelector('.checked .tiret').classList.add("hideimg");
+          box.nextSibling.nextSibling.querySelector('.checked .tiret').classList.remove("showimg");
           // Fill data size button
-          datasize += parseInt(this.dataset.size);
+          datasize += parseInt(this.value);
           document.querySelector('.datasize').innerHTML = datasize/1000;
-          for (k = 0; k < inputs.length; k++) {
-            inputs[k].checked = true;
-          }
+          inputs.forEach(function(input) {
+            input.checked = true;
+          });
         } else {
           // Fill data size button
-          datasize -= parseInt(this.dataset.size);
+          datasize -= parseInt(this.value);
           document.querySelector('.datasize').innerHTML = datasize/1000;
-          for (k = 0; k < inputs.length; k++) {
-            inputs[k].checked = false;
-          }
+          inputs.forEach(function(input) {
+            input.checked = false;
+          });
         }
-      };
-    }
-};
+      });
+    });
+
+    checkbox.forEach(function(box) {
+      var checkDrop = box.parentElement.nextSibling.nextSibling,
+      allChecked = 0;
+      var inputs = checkDrop.querySelectorAll("input");
+      inputs.forEach(function(input){
+        input.addEventListener("change", function() {
+          var contentsize = 0;
+          contentsize = document.querySelector('.datasize').innerHTML*1000;
+          inputs.forEach(function(input){
+            input.checked ? allChecked++ : '';
+            // console.log(box);
+          });
+          if (allChecked === 0) {
+            // Aucun check
+            box.nextSibling.nextSibling.querySelector('.checked .check').classList.add("hideimg");
+            box.nextSibling.nextSibling.querySelector('.checked .tiret').classList.add("hideimg");
+            datasize = contentsize - parseInt(box.value);
+            // contentsize < parseInt(box.value)*1000 ? datasize = ((document.querySelector('.datasize').innerHTML*1000) - (parseInt(box.value)*1000)/1000) : datasize = parseInt(box.value);
+            document.querySelector('.datasize').innerHTML = datasize/1000;
+            box.checked = false;
+          } else if (allChecked === 5) {
+            // Tous check
+            box.nextSibling.nextSibling.querySelector('.checked .check').classList.add("showimg");
+            box.nextSibling.nextSibling.querySelector('.checked .check').classList.remove("hideimg");
+            box.nextSibling.nextSibling.querySelector('.checked .tiret').classList.add("hideimg");
+            box.nextSibling.nextSibling.querySelector('.checked .tiret').classList.remove("showimg");
+          } else {
+            // Plusieurs check
+            box.nextSibling.nextSibling.querySelector('.checked .check').classList.add("hideimg");
+            box.nextSibling.nextSibling.querySelector('.checked .check').classList.remove("showimg");
+            box.nextSibling.nextSibling.querySelector('.checked .tiret').classList.add("showimg");
+            box.nextSibling.nextSibling.querySelector('.checked .tiret').classList.remove("hideimg");
+            console.log(contentsize);
+            console.log(parseInt(box.value));
+            contentsize < parseInt(box.value) ? datasize = ((document.querySelector('.datasize').innerHTML*1000) + (parseInt(box.value)*1000)/1000) : datasize = parseInt(box.value);
+            document.querySelector('.datasize').innerHTML = datasize/1000;
+            box.checked = true;
+          }
+          allChecked = 0;
+        });
+      });
+    });
+
+});
